d3f560d14fd73898ec75b029345f8c36b2b0923a
Mohamed Fazil <mofazil1717@gmail.com>
2022-01-19 23:53:28 +0530

    updated cards of project

--- scripts/projects.js
+++ scripts/projects.js
@@ -14,14 +14,18 @@ function alldata(){
 const addproject =(x) =>{
     const outputfield = document.getElementById("all-projects");
     var card=document.createElement("div");
+    card.classList.add("cards")
+    card.classList.add("g-3")
     card.classList.add("col-lg-4")
     card.classList.add("col-md-6")
-    card.innerHTML=`<img src="https://picsum.photos/200" class="card-img-top" alt="...">
+    card.innerHTML=`<img src="${x.img}" class="card-img-top" alt="...">
                     <div class="card-body">
                     <h5 class="card-title">${x.title}</h5>
                     <p class="card-text">${x.description}</p>
-                    <a href="#" class="btn mr-2"><i class="fas fa-link"></i>${x.viewcode}</a>
-                    <a href="#" class="btn "><i class="fab fa-github"></i>${x.liveproject}</a>
+                    <div class="button">
+                        <a href="#" class="btn "><i class="fas fa-link"></i>LIVE PROJECT</a>
+                        <a href="#" class="btn align-items-end"><i class="fab fa-github"></i>VIEW CODE</a>
+                    </div>
                     </div>`;
     console.log(card)
     outputfield.appendChild(card);
